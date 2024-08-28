4242b497eae12656a412011273d52de63e9ef8a5
Lucas Paranhos <lucasparanhos1998@outlook.com>
2024-08-28 00:01:25 -0300

    add aula 3 realizamos o inicio de uma api

---
+++ Aula1/aula1.js
@@ -0,0 +1,8 @@
+/*function soma (n1,n2){
+    soma = n1 + n2
+    console.log(soma)
+}
+
+soma(4,5)
+*/
+
---
+++ Aula3/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+let alunos = [
+    {id: 1, nome: 'Lucas'},
+    {id: 2, nome: 'Matheus'},
+]
+
+app.get('/api/alunos', (req, res) => {
+    res.json(alunos);
+});
+
+app.get('/api/alunos/getByName/:nome', (req, res) => {
+    const { nome } = req.params;
+    const index = alunos.findIndex(a => a.nome === nome);
+
+    if (index > -1) {
+        res.json(alunos[index]);
+    } else {
+        res.status(404).json({ message: 'Aluno não encontrado'});
+    }
+});
+
+app.post('/api/alunos', (req, res) => {
+    const novoAluno = { id: alunos.length + 1, ...req.body };
+    alunos.push(novoAluno);
+    res.status(201).json(novoAluno);
+});
+
+app.put('/api/alunos/:id', (req, res) => {
+    const { id } = req.params;
+    console.log('--- id', id);
+    const alunoIndex = alunos.findIndex(a => a.id === Number(id));
+    console.log('--- aluno index', alunoIndex);
+    if (alunoIndex > -1) {
+        alunos[alunoIndex] = { id: Number(id), ...req.body };
+        res.json(alunos[alunoIndex]);
+    } else {
+        res.status(404).json({ message: 'Aluno não encontrado'});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}`)
+}) 
