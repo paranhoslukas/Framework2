33eb90b313dbb2d7d71168628df5fbb6376d7b06
Lucas Paranhos <lucasparanhos1998@outlook.com>
2024-08-28 11:38:37 -0300

    chore: Add DELETE endpoint to remove student from API

--- Aula3/index.js
+++ Aula3/index.js
@@ -4,6 +4,11 @@ const PORT = 8080;
 
 app.use(express.json());
 
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}`)
+})  // http://localhost:8080/api/alunos
+
 let alunos = [
     {id: 1, nome: 'Lucas'},
     {id: 2, nome: 'Matheus'},
@@ -43,6 +48,14 @@ app.put('/api/alunos/:id', (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`servidor rodando na porta ${PORT}`)
-}) 
+app.delete('/api/alunos/:id', (req, res) => { 
+    const { id } = req.params;
+    const alunoIndex = alunos.findIndex(a => a.id === Number(id));
+    if (alunoIndex > -1) {
+        alunos.splice(alunoIndex, 1); //O método .splice() em JavaScript é usado para alterar o conteúdo de um array, removendo, substituindo ou adicionando elementos.
+        res.json({ message: 'Aluno removido com sucesso'});
+    } else {
+        res.status(404).json({ message: 'Aluno não encontrado'});
+    }
+}
+)
