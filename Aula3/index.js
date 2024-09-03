4e3546cfa42cff05190230bbe10414d8f96f8743
Lucas Paranhos <lucasparanhos1998@gmail.com>
2024-09-03 20:52:11 -0300

    Aula 5 03/09

--- Aula3/index.js
+++ Aula3/index.js
@@ -5,6 +5,9 @@ const PORT = 8080;
 app.use(express.json());
 
 
+const swaggerUI = require("swagger-ui-express");
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(require('./swagger.json')));
+
 app.listen(PORT, () => {
     console.log(`servidor rodando na porta ${PORT}`)
 })  // http://localhost:8080/api/alunos
